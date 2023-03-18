20fe7085d19d4a2f5b93a3c7d4e27de380beaf51
tmdmini99 <tmdals2451@naver.com>
2023-03-18 10:54:13 +0900

    product 4 option

--- src/main/webapp/WEB-INF/views/product/productOptionAdd.jsp
+++ src/main/webapp/WEB-INF/views/product/productOptionAdd.jsp
@@ -10,17 +10,17 @@
 </head>
 <body>
 <c:import url="../template/header.jsp"></c:import>
-<form action="./productOptionAdd" method="post">
-	<button type="button" class="oi">옵션 네임</button>
-    <div class="op">
-	
-		<div>
-            OptionName:<input type="text" name="optionName">
-            OptionValue :<input type="text" name="optionValue">
-            <button type="button" class="opv">벨류 추가</button>
-            <button type="button" class="opc">새로운 옵션</button>
+<form action="./productOptionAdd" method="post" id="frm">
+    <div class="opi">
+            <button type="button" class="oi">옵션 네임</button>
+        <div class="op">
+        OptionName:<input type="text" name="optionName">
+        OptionValue :<input type="text" name="optionValue">
+        <!-- <button type="button" class="opv">벨류 추가</button> -->
+        <button type="button" class="opc">새로운 옵션</button>
+        <button type="button" class="opd">옵션 삭제</button>
         </div>
-	</div>
+        </div>
 <button type="submit">옵션 생성하기</button>
 </form>
 
--- src/main/webapp/resources/js/productOption.js
+++ src/main/webapp/resources/js/productOption.js
@@ -1,35 +1,77 @@
-$(".op").on("click",".opv",function(){
-    console.log("콘솔");
-    let va='<input type="text" name="optionValue">'
-    $(this).before(va);
-})
-let last = 0;
-let m =2;
-$(".op").on("click",".opc",function(){
-    if(last ==2){
-        alert("옵션은 최대 3개까지만 가능합니다");
-        return;
-    }
-    console.log("콘솔2");
-    let na='<div class="op offset-md-'+m+'">'
+
+const frm = document.getElementById("frm");
+
+let count=0;
+let a= 
+    '<div class="op">'
     +'OptionName:<input type="text" name="optionName">'
     +'OptionValue :<input type="text" name="optionValue">'
-    +'<button type="button" class="opv">벨류 추가</button>'
+    //+'<button type="button" class="opv">벨류 추가</button>'
     +'<button type="button" class="opc">새로운 옵션</button>'
+    +'<button type="button" class="opdd">옵션 삭제</button>'
     +'</div>'
-    $(this).last().after(na);
-    m +=2;
-    last++;
-})
+    
+    
+
 $(".oi").click(function(){
-    let a= '<div class="op">'
-	
-    +'<div>'
+    $(".opi").append(a);
+    count++;
+})
+
+let va='<input type="text" name="optionValue">'
+$(".op").click(function(){
+    console.log("콘솔11111");
+})
+
+$(document).on("click",".opv",function(e){
+    console.log("콘솔");
+    
+    $(this).before(va);
+    
+})
+
+let na=
+    '<div class="op offset-md-2">'
     +'OptionName:<input type="text" name="optionName">'
     +'OptionValue :<input type="text" name="optionValue">'
-    +'<button type="button" class="opv">벨류 추가</button>'
-    +'<button type="button" class="opc">새로운 옵션</button>'
+    //+'<button type="button" class="opv">벨류 추가</button>'
+    +'<button type="button" class="opci">새로운 옵션</button>'
+    +'<button type="button" class="opd">옵션 삭제</button>'
     +'</div>'
+    
+$(document).on("click",".opc",function(e){
+    
+    console.log("콘솔2");
+    
+    $(this).parent().append(na);
+    
+    
+})
+let nas=
+    '<div class="op offset-md-4">'
+    +'OptionName:<input type="text" name="optionName">'
+    +'OptionValue :<input type="text" name="optionValue">'
+   //+'<button type="button" class="opv">벨류 추가</button>'
+   +'<button type="button" class="opdd">옵션 삭제</button>'
     +'</div>'
-    $(".op").after(a);
+$(document).on("click",".opci",function(e){
+   
+    
+    
+    $(this).parent().append(nas);
+    
+   
+})
+$(document).on("click",'.opd',function(){
+    
+
+    $(this).parent().remove();
+    
+})
+$(document).on("click",'.opdd',function(){
+    if(count ==0){
+        alert("옵션은 최소 한개이상 포함되어야합니다");
+    }
+    $(this).parent().remove();
+    count--;
 })
